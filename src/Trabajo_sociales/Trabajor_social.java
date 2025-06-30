9f9b47a967539e9dd36c762b1afb49639c0d9a3c
Kevinq <kevinquiros.m@gmail.com>
2025-06-30 15:30:59 -0600

    Se crea clase Trabajor Social
    Se crea los atributos, metodos y constructor

---
+++ src/Trabajo_sociales/Trabajor_social.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kevin
+ */
+public class Trabajor_social {
+private String cedula; 
+private String nombre;
+private String correo;
+private String telefono;
+private String canton;
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCanton() {
+        return canton;
+    }
+
+    public void setCanton(String canton) {
+        this.canton = canton;
+    }
+
+    public Trabajor_social(String cedula, String nombre, String correo, String telefono, String canton) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.canton = canton;
+    }
+
+
+}
