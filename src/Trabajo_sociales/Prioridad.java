07ed5204695e757feb30440cfec2b96be3b2dfee
Felipe0220 <lusalaspe@est.utn.ac.cr>
2025-06-30 15:46:40 -0600

    Clase Enum Prioridad
    Se creo la clase enum de Prioridad

---
+++ src/Trabajo_sociales/Prioridad.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Trabajo_sociales;
+
+/**
+ *
+ * @author Luisf
+ */
+public enum Prioridad {
+   ALTA("Alta"),
+   MEDIA("Media"),
+   BAJA("Baja");
+   
+   private String prioridad;   
+   
+   private Prioridad(String prioridad){
+       this.prioridad = prioridad;
+    }
+    public String getPrioridad() {
+        return prioridad;
+    }
+    @Override
+    public String toString() {
+        return "Prioridad{" + "ordinal=" + ordinal() + ", name=" + name() + ", prioridad=" + prioridad + '}';
+    }
+}
