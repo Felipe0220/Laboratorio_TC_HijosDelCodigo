b588f9ae8f28c491446185b37438cc4d36514164
Sebas12212005 <Sesalashe@est.utn.ac.cr>
2025-06-30 16:07:08 -0600

    Clase Victima
    Se crea la clase Victima con sus respectivos atributos y metodos para su funcionamiento

---
+++ src/Victimas/UtilDate.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Victimas;
+
+/**
+ *
+ * @author Sebastian
+ */
+class UtilDate {
+
+    static boolean isNotFutureDate(String fechaNaci) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static int calcuEdad(String fechaNaci) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
