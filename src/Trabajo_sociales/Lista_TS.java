dbc81f09b1cf9d6775644b89a3449c7b19706612
Kevinq <kevinquiros.m@gmail.com>
2025-06-30 16:55:22 -0600

    Se actializa Lista de trabajador social
    Se crea el buscador por canton

--- src/Trabajo_sociales/Lista_TS.java
+++ src/Trabajo_sociales/Lista_TS.java
@@ -11,7 +11,8 @@ import Listas.Lista;
  * @author kevin
  */
 public class Lista_TS implements Lista <Trabajador_social>{
-private static String ListaTS[];
+private static Trabajador_social[] lista;
+private int cantidad;
 
     @Override
     public Trabajador_social buscar(Object id) {
@@ -19,8 +20,23 @@ private static String ListaTS[];
     }
 
     @Override
-    public int contar() {
+    public boolean eliminar(Trabajador_social v) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-}
+    @Override
+    public boolean agregar(Trabajador_social v) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+
+    public Trabajador_social[] buscarPorCanton(String canton) {
+        int count = 0;
+        for (int i = 0; i < cantidad; i++) {
+            if (lista[i].getCanton().equalsIgnoreCase(canton)) {
+                count++;
+            }
+        }
+    return null;
+
+    }
