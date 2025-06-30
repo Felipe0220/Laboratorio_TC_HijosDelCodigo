b292a27ece014d534f33cefde47605066919a5e1
Kevinq <kevinquiros.m@gmail.com>
2025-06-30 16:35:57 -0600

    Se crea la clase Lista TS
    Se hace la implemtacion

---
+++ src/Trabajo_sociales/Lista_TS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Trabajo_sociales;
+
+import Listas.Lista;
+
+/**
+ *
+ * @author kevin
+ */
+public class Lista_TS implements Lista <Trabajador_social>{
+private static String ListaTS[];
+
+    @Override
+    public Trabajador_social buscar(Object id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int contar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
