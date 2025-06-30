7777c34a868016dd51b060b9faa30a422a73a00e
DavidSalazar <jdrsalazar1025@gmail.com>
2025-06-30 16:34:45 -0600

    se crea la clase CasoList
    clase incompleta

---
+++ src/Casos/CasoList.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Casos;
+
+import Listas.Lista;
+
+/**
+ *
+ * @author UTN
+ */
+public class CasoList implements Lista<Caso> {
+    private static String Casos[];
+    @Override
+    public Caso buscar(Object id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int contar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
